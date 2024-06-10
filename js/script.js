e246e4b228ab5e3856ff5dff9bb3278b25ab4291
May <26matmoran@gmail.com>
2024-06-10 11:09:31 -0600

    Add limited func to do basic calculations + - / *
    If you click a number, then an operator, then the other number
    and finally the equal sign, everything works.
    
    If you do things in an other order then it may break.

--- js/script.js
+++ js/script.js
@@ -18,6 +18,7 @@ function divide(num1, num2) {
 let num1 = "";
 let operator = "+";
 let num2 = "";
+let answer = "0";
 
 function operate(num1, num2, operator) {
     switch (operator) {
@@ -38,30 +39,88 @@ let digits = document.querySelectorAll(".digit");
 let operators = document.querySelectorAll(".operator");
 let backArrow = document.querySelector(".back-arrow");
 let display = document.querySelector(".display span");
+let operationToDo = "add";
+let numberBeingEntered = "num1";
+let equalSign = document.querySelector(".equal");
 
 digits.forEach(digit => {
     digit.addEventListener("click", () => {
-        display.style.color = "white";
-        num1 += digit.textContent;
-        display.textContent = num1;
+        if (numberBeingEntered === "num1") {
+            display.style.color = "white";
+            num1 += digit.textContent;
+            display.textContent = num1;
+        } else {
+            display.style.color = "white";
+            num2 += digit.textContent;
+            display.textContent = num2;
+        }
     }) 
 })
 
 operators.forEach(operator => {
     operator.addEventListener("click", () => {
-        console.log(operator.textContent);
+        switch (operator.textContent) {
+            case "+":
+                operationToDo = "addition"
+                break;
+            case "-":
+                operationToDo = "subtraction"
+                break;
+            case "÷":
+                operationToDo = "division"
+                break;
+            case "x":
+                operationToDo = "multiplication"
+                break;
+        }
+        display.textContent = "0000000000";
+        display.style.color = "rgb(176, 209, 221)";
+        numberBeingEntered = "num2";
     })
 })
 
 backArrow.addEventListener("click", () => {
-    if (num1.length === 1) {
-        num1 = "";
-        display.style.color = "rgb(176, 209, 221)";
-        display.textContent = "0000000000";
+    if (numberBeingEntered === "num1") {
+        if (num1.length === 1) {
+            num1 = "";
+            display.style.color = "rgb(176, 209, 221)";
+            display.textContent = "0000000000";
+        } else {
+            let arrayOfNum1 = num1.split("");
+            arrayOfNum1.pop();
+            num1 = arrayOfNum1.join("");
+            display.textContent = num1;
+        }
     } else {
-        let arrayOfNum1 = num1.split("");
-        arrayOfNum1.pop();
-        num1 = arrayOfNum1.join("");
-        display.textContent = num1;
+        if (num2.length === 1) {
+            num2 = "";
+            display.style.color = "rgb(176, 209, 221)";
+            display.textContent = "0000000000";
+        } else {
+            let arrayOfNum2 = num2.split("");
+            arrayOfNum2.pop();
+            num2 = arrayOfNum2.join("");
+            display.textContent = num2;
+        }
     }
+})
+
+equalSign.addEventListener("click", () => {
+    num1 = +num1;
+    num2 = +num2;
+    switch (operationToDo) {
+        case "addition":
+            answer = add(num1, num2);
+            break;
+        case "subtraction":
+            answer = subtract(num1, num2);
+            break;
+        case "multiplication":
+            answer = multiply(num1, num2);
+            break;
+        case "division":
+            answer = divide(num1, num2);
+            break;
+    }
+    display.textContent = answer;
 })
