caeb00778afc6e01786996ab4cce9ab69f9c0e42
May <26matmoran@gmail.com>
2024-06-11 13:07:28 -0600

    Fix bugs - divide by 0 + entering an operator 1st
    1. Divide by 0 would give you infinity, now error
    2. Entering an operator first would not give you an error per say
    but it would remain in a bluish color until a # was pressed. Now when
    you click an operator it automatically changes to white.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-// add ans button, and a more reactive display.
-// repeating calculations (if i click 2 + 2, then click equals it will give me 4, if i click equal again it should give me six.) 
-// Add more indications. Like for example color differently the operator being used. add keywords like ans and stuff like that.
+// give func to ans button
+// give func to decimal button
+// add arrows to see complete answer
+// add keyboard func
 
 
 function add(num1, num2) {
@@ -16,7 +17,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    if (num2 === 0) {
+    if (num2 === "0") {
         return "Error";
     }
     return num1 / num2;
@@ -28,20 +29,6 @@ let operator = "";
 let num2 = "";
 let answer = "";
 let completeExpression = "";
-
-function operate(num1, num2, operator) {
-    switch (operator) {
-        case "+":
-            return add(num1, num2);
-        case "-":
-            return subtract(num1, num2);
-        case "/":
-            return divide(num1, num2);
-        case "*":
-            return multiply(num1, num2);
-    }
-}
-
 
 let digits = document.querySelectorAll(".digit");
 let operators = document.querySelectorAll(".operator");
@@ -91,6 +78,7 @@ backspaceButton.addEventListener("click", () => {
 
 operators.forEach(operator => {
     operator.addEventListener("click", () => {
+        display.style.color = "white";
         if (completeExpression[completeExpression.length-1] === "+" ||
         completeExpression[completeExpression.length-1] === "-" || 
         completeExpression[completeExpression.length-1] === "x" || 
